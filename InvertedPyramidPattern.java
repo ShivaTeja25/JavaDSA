a8c6aca2c4619e9f95bc40681dfdc9a464d14333
Shiva Teja <shivateja252709@gmail.com>
2022-11-06 23:37:52 +0530

    Create InvertedPyramidPattern.java

---
+++ InvertedPyramidPattern.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class InvertedPyramidPattern{   
+    
+   public static void main(String[] args)     
+   {    
+      
+	Scanner sc = new Scanner(System.in);
+       
+	int n = sc.nextInt();    
+           
+       for (int i = n; i != 0; i--)     
+       {    
+           for (int j = 0; j<n-i; j++)    
+           {    
+               System.out.print(" ");    
+           }    
+           for (int k = 0; k < (2 * i - 1); k++)     
+           {    
+               System.out.print("*");    
+           }    
+           System.out.println();    
+   	 }    
+   }    
+}
